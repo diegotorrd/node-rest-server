d7bf818c5281676aaa4e00b16ae6ecf21f5f426b
Diego <diego40615@gmail.com>
2021-04-15 00:18:21 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,22 @@
 //========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//========================
+// ENTORNO
+//========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || "dev"; //env de heroku
+
+//========================
+// BASE DE DATOS
+//========================
+if (process.env.NODE_ENV === "dev") {
+  urlDB =
+    "mongodb+srv://generalUser:WM2ohphEjekZQhlj@cluster0.pc6ee.mongodb.net/cafe?retryWrites=true&w=majority";
+} else {
+  urlDB =
+    "mongodb+srv://generalAppUsua:lucuma2020@cluster0.a8kcm.mongodb.net/cafe?retryWrites=true&w=majority";
+}
+
+process.env.urlDB = urlDB;
